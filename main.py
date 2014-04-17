79e65971210d6873591b14fc57c05ca8d4057834
Alon Swartz <alon@turnkeylinux.org>
2014-04-17 13:44:21 +0300

    set DEBUG flag on the fly (development == True, production == False)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import jinja2
 import webapp2
 
+SERVER_SOFTWARE = os.environ.get('SERVER_SOFTWARE')
+DEBUG = True if SERVER_SOFTWARE.startswith('Development') else False
+
 TEMPLATES_PATH = os.path.join(os.path.dirname(__file__), 'templates')
 JINJA = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_PATH))
 
@@ -14,5 +17,5 @@ class Main(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', Main),
-], debug=True)
+], debug=DEBUG)
 
